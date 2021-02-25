3bdbfee7e200fce0027e481800b89e4511dd5dab
nilldiggonto <nilldiggonto@gmail.com>
2021-02-25 17:20:50 +0600

    All Projects List

--- my_projects/my_projects/urls.py
+++ my_projects/my_projects/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
-from .views import home_view
+from .views import home_view,project_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home_view,name='homepage'),
+    path('project/list/',project_list,name='project-list'),
 
     path('social',include('social_project.social_app.urls')),
 ]
--- my_projects/my_projects/views.py
+++ my_projects/my_projects/views.py
@@ -4,3 +4,7 @@ def home_view(request):
     template_name = 'my_projects/home.html'
 
     return render(request,template_name)
+
+def project_list(request):
+    template_name ='my_projects/project_list.html'
+    return render(request,template_name)
