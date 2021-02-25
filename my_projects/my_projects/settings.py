e16bdafad97217fb0b90cafd3ca82ec359597f46
nilldiggonto <nilldiggonto@gmail.com>
2021-02-25 17:16:43 +0600

    Logout social

--- my_projects/my_projects/settings.py
+++ my_projects/my_projects/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGOUT_REDIRECT_URL = 'homepage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- my_projects/social_project/social_app/urls.py
+++ my_projects/social_project/social_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import social_frontpage,signupView
+from django.contrib.auth import views
 
 urlpatterns = [
     path('',social_frontpage,name='social-home'),
     path('signup/',signupView,name='social-signup'),
+
+    path('logout/',views.LogoutView.as_view(),name='social-logout'),
 ]
