6a738a1ffe099724335891d4c43687c83d1a3db7
nilldiggonto <nilldiggonto@gmail.com>
2021-02-25 21:20:19 +0600

    Vue js

--- my_projects/social_project/feed/models.py
+++ my_projects/social_project/feed/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Social(models.Model):
+    body        = models.CharField(max_length=300)
+    created_by  = models.ForeignKey(User,on_delete=models.CASCADE,related_name='socials')
+    created_at  = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
