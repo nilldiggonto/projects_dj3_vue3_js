e9ff934f5993ebf6eaacd93dfaf3c566270a2fad
nilldiggonto <nilldiggonto@gmail.com>
2021-02-25 23:25:38 +0600

    Follw & follower

--- my_projects/social_project/social_profile/urls.py
+++ my_projects/social_project/social_profile/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import socialprofileView
+from .views import socialprofileView,follow_userView,unfollow_userView,followerView,followView
 
 urlpatterns = [
     path('<str:username>',socialprofileView,name='social-profile'),
+    path('<str:username>/follow/',follow_userView,name='social-user-profile-follow'),
+    path('<str:username>/unfollow/',unfollow_userView,name='social-user-profile-unfollow'),
+    path('<str:username>/follower/list/',followerView,name='social-follower-list'),
+    path('<str:username>/follows/list/',followView,name='social-follow-list'),
+
+
 ]
--- my_projects/social_project/social_profile/views.py
+++ my_projects/social_project/social_profile/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def socialprofileView(request,username):
     user = get_object_or_404(User,username=username)
 
@@ -8,5 +10,41 @@ def socialprofileView(request,username):
         'user':user,
     }
     template_name = 'social_profile/social_profile.html'
+
+    return render(request,template_name,context)
+
+@login_required
+def follow_userView(request,username):
+    user = get_object_or_404(User,username=username)
+
+    request.user.SocialProfile.follows.add(user.SocialProfile)
+    return redirect('social-profile', username=username)
+
+@login_required
+def unfollow_userView(request,username):
+    user = get_object_or_404(User,username=username)
+
+    request.user.SocialProfile.follows.remove(user.SocialProfile)
+    return redirect('social-profile', username=username)
+
+
+@login_required
+def followerView(request,username):
+    user = get_object_or_404(User,username=username)
+    template_name = 'social_profile/social_follower.html'
+
+    context = {'user':user,}
+
+
+    return render(request,template_name,context)
+
+
+@login_required
+def followView(request,username):
+    user = get_object_or_404(User,username=username)
+    template_name = 'social_profile/social_follow.html'
+
+    context = {'user':user,}
+
 
     return render(request,template_name,context)
