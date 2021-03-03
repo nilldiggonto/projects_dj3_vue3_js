de154681c8cfd3647ca3db73a8db17f2ddc5e81d
nilldiggonto <nilldiggonto@gmail.com>
2021-03-04 02:17:50 +0600

    converse

--- my_projects/social_project/social_conversation/urls.py
+++ my_projects/social_project/social_conversation/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import conversationView
+from .views import conversationView,conversationRecipient
 
 urlpatterns = [
     path('',conversationView,name='social-converse'),
+    path('chat/<int:user_id>/',conversationRecipient,name='social-converse-all'),
 ]
--- my_projects/social_project/social_conversation/views.py
+++ my_projects/social_project/social_conversation/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from .models import Conversation,ConversationMessage
+from django.contrib.auth.models import User
 # Create your views here.
 
 @login_required
@@ -13,3 +14,22 @@ def conversationView(request):
         'converse':converse,
     }
     return render(request,tempalte_name,context)
+
+
+@login_required
+def conversationRecipient(request,user_id):
+    template_name = 'conversation/conversation.html'
+    conversations = Conversation.objects.filter(users__in=[request.user.id])
+    conversations = conversations.filter(users__in=[user_id])
+
+    if conversations.count() ==1:
+        conversation = conversations[0]
+    
+    else:
+        recipient = User.objects.get(pk=user_id)
+        conversation = Conversation.objects.create()
+        conversation.users.add(request.user)
+        conversation.users.add(recipient)
+        conversation.save()
+    return render(request,template_name,{'conversation':conversation})
+
