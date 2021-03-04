5f1f7ee77533135b9fa885cfed1caaf1042db526
nilldiggonto <nilldiggonto@gmail.com>
2021-03-04 12:58:38 +0600

    Conversation Done

--- my_projects/social_project/social_conversation/urls.py
+++ my_projects/social_project/social_conversation/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import conversationView,conversationRecipient
+from .api import api_add_message
 
 urlpatterns = [
     path('',conversationView,name='social-converse'),
     path('chat/<int:user_id>/',conversationRecipient,name='social-converse-one'),
+
+    path('api/add_message/',api_add_message,name='social-add-message'),
 ]
