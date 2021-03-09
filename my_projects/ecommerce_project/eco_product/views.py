3928580b79ae385871bce16a9f5aef01c301c2fe
nilldiggonto <nilldiggonto@gmail.com>
2021-03-09 20:32:24 +0600

    product eco details

--- my_projects/ecommerce_project/eco_product/views.py
+++ my_projects/ecommerce_project/eco_product/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+import random
 
 # Create your views here.
+from .models import Category,Product
+
+def productDetail(request,category_slug,product_slug):
+    product = get_object_or_404(Product,category__slug=category_slug,slug =product_slug)
+
+    similar_products = list(product.category.products.exclude(id=product.id))
+
+    if (len(similar_products)>4):
+        similar_products = random.sample(similar_products,4)
+    
+    template_name = 'product/product.html'
+    context = {
+        'product':product,
+        'similar_products':similar_products,
+    }
+    return render(request,template_name,context)
+
+    
--- my_projects/ecommerce_project/ecommerce_app/views.py
+++ my_projects/ecommerce_project/ecommerce_app/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from ecommerce_project.eco_vendor.models import Vendor
+from ecommerce_project.eco_product.models import Product 
+
+
 
 # Create your views here.
-@login_required
+# @login_required
 def ecoView(request):
     template_name= 'ecommerce/eco.html'
+    products = Product.objects.all()[0:8]
     # if 
     vendor = None
-    if Vendor.objects.filter(created_by=request.user).exists():
-        vendor = Vendor.objects.get(created_by=request.user)
-        # print(vendor)
-        # print(vendor)
-        # print(vendor)
-        # print(vendor)
-        # print(vendor)
-        # print(vendor)
-    # else:
+    if request.user.is_authenticated:
+        if Vendor.objects.filter(created_by=request.user).exists():
+            vendor = Vendor.objects.get(created_by=request.user)
+            # print(vendor)
+            # print(vendor)
+            # print(vendor)
+            # print(vendor)
+            # print(vendor)
+            # print(vendor)
+        # else:
 
-    return render(request,template_name,{'vendor':vendor})
+    return render(request,template_name,{'vendor':vendor,'products':products})
