6f7d70b167af93f4e05c355ede13eee3e9e0bff3
nilldiggonto <nilldiggonto@gmail.com>
2021-03-31 01:42:21 +0600

    Update Account

--- my_projects/time_track_project/time_track/urls.py
+++ my_projects/time_track_project/time_track/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import time_frontpage,time_account
+from .views import time_frontpage,time_account,time_edit_profile
 
 urlpatterns = [
     path('',time_frontpage,name='time-home'),
     path('time-account/',time_account,name='time-account'),
+    path('edit/time-account/',time_edit_profile,name='time-edit-account'),
 ]
--- my_projects/time_track_project/time_track/views.py
+++ my_projects/time_track_project/time_track/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
+
 def time_frontpage(request):
     tempalte_name = 'time_track/time_front.html'
 
@@ -15,4 +17,21 @@ def time_account(request):
         'user':user
     }
     return render(request,template_name,context)
+
+@login_required
+def time_edit_profile(request):
+    template_name = 'time_track/time_edit_profile.html'
+    user = request.user
+
+
+    if request.method == 'POST':
+        request.user.first_name = request.POST.get('first_name')
+        request.user.last_name = request.POST.get('last_name')
+        request.user.email = request.POST.get('email','')
+        request.user.save()
+
+        messages.info(request,'Profile edit successfull')
+        return redirect('time-account')
+    return render(request,template_name,{'user':user})
+
 
