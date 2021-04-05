1549f4fa4db326427d14ed8eafb0ea4b07fb3c21
nilldiggonto <nilldiggonto@gmail.com>
2021-04-05 18:22:37 +0600

    Edit team info

--- my_projects/time_track_project/time_team/urls.py
+++ my_projects/time_track_project/time_team/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import add_team,team
+from .views import add_team,team,edit_team
 
 urlpatterns = [
     path('add/',add_team,name='time-add-team'),
+    path('edit/',edit_team,name='time-edit-team'),
     path('<int:team_id>/',team,name='time-team-details'),
+
 ]
--- my_projects/time_track_project/time_team/views.py
+++ my_projects/time_track_project/time_team/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
-
+from django.contrib import messages
 # Create your views here.
 from .models import Team
 from django.contrib.auth.decorators import login_required
@@ -28,4 +28,19 @@ def team(request,team_id):
     team = get_object_or_404(Team,pk=team_id,status=Team.ACTIVE,members__in=[request.user])
     tempalte_name ='team/team.html'
     return render(request,'team/team.html',{'team':team})
+
+@login_required
+def edit_team(request):
+    team = get_object_or_404(Team,pk=request.user.timeprofile.active_team_id,status=Team.ACTIVE,members__in=[request.user])
+    # team = get_object_or_404(Team,pk=request.user.timeprofile.active_team_id,status=Team.ACTIVE,)
+    template_name = 'team/edit.html'
+    if request.method == 'POST':
+        title = request.POST.get('title')
+
+        if title:
+            team.title = title
+            team.save()
+            messages.info(request,'Saved changes')
+            return redirect('time-team-details', team_id=team.id)
+    return render(request,template_name,{'team':team})
 
