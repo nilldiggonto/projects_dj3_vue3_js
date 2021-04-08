2ada35c669732db40b433a336721e329685512fa
nilldiggonto <nilldiggonto@gmail.com>
2021-04-08 20:36:17 +0600

    time task list&detail view

--- my_projects/time_track_project/time_task/urls.py
+++ my_projects/time_track_project/time_task/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import time_projects
+from .views import time_projects,time_projectDetail
 
 urlpatterns = [
     path('',time_projects,name='time-projects-list'),
+    path('detail/<int:project_id>/',time_projectDetail,name='time-project-detail'),
 ]
--- my_projects/time_track_project/time_task/views.py
+++ my_projects/time_track_project/time_task/views.py
@@ -13,4 +13,26 @@ def time_projects(request):
     team = get_object_or_404(Team,pk=request.user.timeprofile.active_team_id,status=Team.ACTIVE)
     projects = team.projects.all()
 
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        print(title)
+
+        if title:
+            project = ProjectTask.objects.create(team=team,title=title,created_by=request.user)
+
+            return redirect('time-projects-list')
+
     return render(request,template_name,{'team':team,'projects':projects})
+    
+
+@login_required
+def time_projectDetail(request,project_id):
+    team = get_object_or_404(Team,pk=request.user.timeprofile.active_team_id,status=Team.ACTIVE)
+    project = get_object_or_404(ProjectTask,team=team,pk=project_id)
+
+    template_name = 'project/project_detail.html'
+    context = {
+        'team':team,
+        'project':project
+    }
+    return render(request,template_name,context)
