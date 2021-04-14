24125e9372f6cf0816e649e48d9c654447162503
nilldiggonto <nilldiggonto@gmail.com>
2021-04-14 19:18:39 +0600

    Time for Month added

--- my_projects/time_track_project/time_dashboard/views.py
+++ my_projects/time_track_project/time_dashboard/views.py
@@ -29,6 +29,16 @@ def timeDashboard(request):
     date_user = datetime.now()- timedelta(days = num_days)
     date_entries = Entry.objects.filter(team=team,created_by=request.user,created_at__date=date_user,is_track=True)
 
+
+    #month pagination
+    user_num_months = int(request.GET.get('user_num_months',0))
+    user_month = datetime.now()-relativedelta(months=user_num_months)
+
+    for project in all_projects:
+        project.get_time_for_user_and_project_and_month = get_time_for_user_and_project_and_month(team,project,request.user,user_month)
+
+    
+
     context = {
         'team':team,
         'all_projects':all_projects,
@@ -36,6 +46,13 @@ def timeDashboard(request):
         'num_days':num_days,
         'date_user':date_user,
         'members':members,
+        'time_for_user_and_date':get_time_for_user_and_date(team,request.user,date_user),
+
+        'user_num_months':user_num_months,
+        'user_month':user_month,
+        'time_for_user_and_month':get_time_for_uer_and_month(team,request.user,user_month),
         'time_for_user_and_date':get_time_for_user_and_date(team,request.user,date_user)
+
+
     }
     return render(request,template_name,context)
